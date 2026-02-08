4fd5ff97654033d0962225120f2f52600a5f5e52
Anshul Bharat <anshul.ab17x@gmail.com>
2026-02-08 18:23:39 +0530

    adding nav

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,8 +5,8 @@ export default function Navbar() {
     <nav className="sticky top-0 z-50 backdrop-blur-xl bg-[#0f1117]/80 border-b border-white/10">
       <div className="flex justify-center gap-12 py-6">
         <a href="/" className="hover:text-gray-300">Portfolio</a>
-        {/* <a href="/about" className="hover:text-gray-300">About</a>
-        <a href="/blog" className="hover:text-gray-300">Blog</a> */}
+        <a href="/about" className="hover:text-gray-300">About</a>
+        <a href="/blog" className="hover:text-gray-300">Blog</a>
       </div>
     </nav>
   )
