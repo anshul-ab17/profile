9d202126baa5caecbca435098021f15886631f9f
Anshul Bharat <anshul.ab17x@gmail.com>
2026-02-08 17:37:28 +0530

    removing deploy.yml

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: isProd ? "/profile" : "",
-  assetPrefix: isProd ? "/profile/" : "",
   images: {
     unoptimized: true,
   },
